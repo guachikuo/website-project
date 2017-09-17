4a700a7ec20b6d2628f6990d98300a65a7f6fb58
kuoandy1 <kuoandy1@gmail.com>
2017-09-18 04:39:02 +0800

    hw4_final

--- hw4/js/script.js
+++ hw4/js/script.js
@@ -1,28 +1,50 @@
 $(document).ready(function(){
-	var api = "https://api.twitch.tv/kraken/streams/";
-	var game = "League%20of%20Legends" ;
-	var client_id = "6515p95jb9xihs3dg4w1d05ck6vy8n";
-	var limit = 20;
-	var apiURL = api + "?game=" + game + "&client_id=" + client_id + "&limit=" + limit; 
-	$.ajax({
-		method: "GET",
-		url: apiURL,
-  		success: function(data) {
-  			for(i=0;i<limit;i++){
-  				var link = data.streams[i].channel.url;
-  				var preview = data.streams[i].preview.medium;
-  				var logo = data.streams[i].channel.logo;
-  				var channel_name = data.streams[i].channel.status;
-  				var display_name = data.streams[i].channel.display_name;
-    			$('.row').append(
-    				'<div class="broadcast">' +
+	/*抓資料*/
+	function getApi(call_back){
+		var api = "https://api.twitch.tv/kraken/streams/";
+		var game = "League%20of%20Legends" ;
+		const client_id = "6515p95jb9xihs3dg4w1d05ck6vy8n";
+		var limit = 20;
+		var apiURL = api + "?game=" + game + "&client_id=" + client_id + "&limit=" + limit; 
+		$.ajax({
+			method: "GET",
+			url: apiURL,
+	  		success: function(data) {
+	  			call_back(null,data);
+	  		},
+	  		error: function(err){
+				call_back(err,null);
+	  		}
+		});
+	}
+
+	/*做處理*/
+	getApi(function(err,data){
+		if(err)
+			console.log(err);
+		else{
+			for(i=0;i<data.streams.length;i++){
+		    	$('.row').append(getBroadcast(data));
+		    }
+		    $('.row').append('<div class="broadcast"></div>');
+		}
+	});
+
+	/*append到html*/
+	function getBroadcast(data){
+		const link = data.streams[i].channel.url;
+  		const preview = data.streams[i].preview.medium;
+  		const logo = data.streams[i].channel.logo;
+  		const channel_name = data.streams[i].channel.status;
+  		const display_name = data.streams[i].channel.display_name;
+		return '<div class="broadcast">' +
     					'<a href="' + link + '" target="_blank">' +
 							'<div class="channel">' +
-								'<img src="' + preview +'">' +
+								'<img src="' + preview +'"/>' +
 							'</div>' +
 							'<div class="twitcher">' +
 								'<div class="pic_box">' +
-									'<img src="' + logo + '">' +
+									'<img src="' + logo + '"/>' +
 								'</div>' +
 								'<div class="info">' +
 									'<div class="twitch_channel">' + channel_name + '</div>' +
@@ -30,10 +52,6 @@ $(document).ready(function(){
 								'</div>' +
 							'</div>' +
 						'</a>' +
-					'</div>'	
-    			);
-    		}
-    		$('.row').append('<div class="broadcast"></div>');
-  		}
-	});
+					'</div>';
+	}
 });

8c08ae4f511bb1cee9eb66460cc8d54a2de0eee9
kuoandy1 <kuoandy1@gmail.com>
2017-09-18 17:46:18 +0800

    hw5

--- js/script.js
+++ js/script.js
@@ -1,57 +1,74 @@
-$(document).ready(function(){
-	/*抓資料*/
-	function getApi(call_back){
-		var api = "https://api.twitch.tv/kraken/streams/";
-		var game = "League%20of%20Legends" ;
-		const client_id = "6515p95jb9xihs3dg4w1d05ck6vy8n";
-		var limit = 20;
-		var apiURL = api + "?game=" + game + "&client_id=" + client_id + "&limit=" + limit; 
-		$.ajax({
-			method: "GET",
-			url: apiURL,
-	  		success: function(data) {
-	  			call_back(null,data);
-	  		},
-	  		error: function(err){
-				call_back(err,null);
-	  		}
-		});
-	}
+let index = 0
+let isLoading = false;
 
-	/*做處理*/
+/*抓資料*/
+function getApi(call_back){
+	var api = "https://api.twitch.tv/kraken/streams/";
+	var game = "League%20of%20Legends" ;
+	var client_id = "6515p95jb9xihs3dg4w1d05ck6vy8n";
+	var limit = 9;
+	var apiURL = api + "?game=" + game + "&client_id=" + client_id + "&limit=" + limit + "&offset=" + index; 
+	isLoading = true;
+	$.ajax({
+		method: "GET",
+		url: apiURL,
+	  	success: function(data) {
+	  		call_back(null,data);
+	  	},
+	  	error: function(err){
+			call_back(err,null);
+	  	}
+	});
+}
+
+/*做處理*/
+function getdata(){
 	getApi(function(err,data){
 		if(err)
 			console.log(err);
 		else{
 			for(i=0;i<data.streams.length;i++){
-		    	$('.row').append(getBroadcast(data));
-		    }
-		    $('.row').append('<div class="broadcast"></div>');
+			    $('.row').append(getBroadcast(data));
+			}
 		}
+		index+=9;
+		isLoading = false;
 	});
+}
 
-	/*append到html*/
-	function getBroadcast(data){
-		const link = data.streams[i].channel.url;
-  		const preview = data.streams[i].preview.medium;
-  		const logo = data.streams[i].channel.logo;
-  		const channel_name = data.streams[i].channel.status;
-  		const display_name = data.streams[i].channel.display_name;
-		return '<div class="broadcast">' +
-    					'<a href="' + link + '" target="_blank">' +
-							'<div class="channel">' +
-								'<img src="' + preview +'"/>' +
-							'</div>' +
-							'<div class="twitcher">' +
-								'<div class="pic_box">' +
-									'<img src="' + logo + '"/>' +
-								'</div>' +
-								'<div class="info">' +
-									'<div class="twitch_channel">' + channel_name + '</div>' +
-									'<div class="twitcher_name">' + display_name + '</div>' +
-								'</div>' +
-							'</div>' +
-						'</a>' +
-					'</div>';
-	}
+
+/*append到html*/
+function getBroadcast(data){
+	var link = data.streams[i].channel.url;
+  	var preview = data.streams[i].preview.medium;
+  	var logo = data.streams[i].channel.logo;
+  	var channel_name = data.streams[i].channel.status;
+  	var display_name = data.streams[i].channel.display_name;
+	return '<div class="broadcast">' +
+    			'<a href="' + link + '" target="_blank">' +
+					'<div class="channel">' +
+						'<img src="' + preview + '" onload="this.style.opacity=1" />' +
+					'</div>' +
+					'<div class="twitcher">' +
+						'<div class="pic_box">' +
+							'<img src="' + logo + '" onload="this.style.opacity=1"/>' +
+						'</div>' +
+						'<div class="info">' +
+							'<div class="twitch_channel">' + channel_name + '</div>' +
+							'<div class="twitcher_name">' + display_name + '</div>' +
+						'</div>' +
+					'</div>' +
+				'</a>' +
+			'</div>';
+}
+
+$(function(){
+	getdata();
+	$(window).scroll(function() {
+   		if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+       		if(!isLoading){
+       			getdata();
+       		}
+   		}
+   	});
 });
